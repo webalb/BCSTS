e778539e41ff761fc9136af507299427a4d67a42
Auwal Gurama <auwalgurama30@gmail.com>
2025-03-07 11:18:51 +0100

    setting static root

--- bcsts/settings.py
+++ bcsts/settings.py
@@ -142,6 +142,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'), 
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where collected static files will be stored
 
 
 # Default primary key field type
